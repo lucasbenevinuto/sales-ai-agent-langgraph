d32b34143cc05af264bb208a52f3ac08d8d34e6c
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-12-19 18:45:29 -0300

    graph first commit

---
+++ virtual_sales_agent/graph.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from typing import Annotated
+
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import AnyMessage, add_messages
+from langgraph.prebuilt import tools_condition
+from typing_extensions import TypedDict
+
+from virtual_sales_agent.tools import (
+    check_order_status,
+    create_order,
+    search_products,
+    search_products_recommendations,
+)
+from virtual_sales_agent.utils import create_tool_node_with_fallback
+
+load_dotenv()
+
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            configuration = config.get("configurable", {})
+            customer_id = configuration.get("customer_id", None)
+            state = {**state, "user_info": customer_id}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+
+llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview", temperature=0)
+
+assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful virtual sales agent. "
+            " Use the provided tools to search for products, create orders, create order status and recommend products to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
+tools = [
+    search_products,
+    search_products_recommendations,
+    create_order,
+    check_order_status,
+]
+
+assistant_runnable = assistant_prompt | llm.bind_tools(tools)
+
+builder = StateGraph(State)
+
+
+# Define nodes: these do the work
+builder.add_node("assistant", Assistant(assistant_runnable))
+builder.add_node("tools", create_tool_node_with_fallback(tools))
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+# The checkpointer lets the graph persist its state
+# this is a complete memory for the entire graph.
+memory = MemorySaver()
+graph = builder.compile(checkpointer=memory)
