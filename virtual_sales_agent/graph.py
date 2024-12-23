5e4daf1ea2c63fc0525d95b771e82a0dc949917b
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-12-23 14:32:56 -0300

    split tools into senstive (need confirmation) and safe tools (no need for confirmation)

--- virtual_sales_agent/graph.py
+++ virtual_sales_agent/graph.py
@@ -120,32 +120,54 @@ If you can't find exactly what the customer is looking for, explore alternatives
     ]
 ).partial(time=datetime.now)
 
-
-tools = [
+# "Read"-only tools
+safe_tools = [
     get_available_categories,
     search_products,
     search_products_recommendations,
-    create_order,
     check_order_status,
 ]
 
-assistant_runnable = assistant_prompt | llm.bind_tools(tools)
+# Sensitive tools (confirmation needed)
+sensitive_tools = [
+    create_order,
+]
+
+sensitive_tool_names = {tool.name for tool in sensitive_tools}
+
+assistant_runnable = assistant_prompt | llm.bind_tools(safe_tools + sensitive_tools)
 
 builder = StateGraph(State)
 
 
 # Define nodes: these do the work
 builder.add_node("assistant", Assistant(assistant_runnable))
-builder.add_node("tools", create_tool_node_with_fallback(tools))
+builder.add_node("safe_tools", create_tool_node_with_fallback(safe_tools))
+builder.add_node("sensitive_tools", create_tool_node_with_fallback(sensitive_tools))
+
+
+def route_tools(state: State):
+    next_node = tools_condition(state)
+    # If no tools are invoked, return to the user
+    if next_node == END:
+        return END
+    ai_message = state["messages"][-1]
+    # This assumes single tool calls. To handle parallel tool calling, you'd want to
+    # use an ANY condition
+    first_tool_call = ai_message.tool_calls[0]
+    if first_tool_call["name"] in sensitive_tool_names:
+        return "sensitive_tools"
+    return "safe_tools"
+
+
 # Define edges: these determine how the control flow moves
 builder.add_edge(START, "assistant")
 builder.add_conditional_edges(
-    "assistant",
-    tools_condition,
+    "assistant", route_tools, ["safe_tools", "sensitive_tools", END]
 )
-builder.add_edge("tools", "assistant")
+builder.add_edge("safe_tools", "assistant")
+builder.add_edge("sensitive_tools", "assistant")
 
-# The checkpointer lets the graph persist its state
-# this is a complete memory for the entire graph.
+# Compile the graph
 memory = MemorySaver()
-graph = builder.compile(checkpointer=memory, interrupt_before=["tools"])
+graph = builder.compile(checkpointer=memory, interrupt_before=["sensitive_tools"])
